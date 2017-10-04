2e8180f5b64f8f0884b45ada88e823de9d93e934
Pavel Shlyak <shlyak-pavel2011@ya.ru>
2017-10-04 17:42:08 +0300

    1st Laboratory project

---
+++ alg/lab1.pas
@@ -0,0 +1,76 @@
+
+Program Project1;
+
+Const  lmax = 200;
+
+Type  mas = array[1..lmax] Of real;
+
+Var 
+  n, i, first_negative, curr_min, size_new: integer;
+  x, h, a: real;
+  r: mas;
+Begin
+  Writeln('Laboratory work no. 1');
+  Writeln('Ex. 1');
+  {The task number one}
+  Repeat
+    Write('Enter array length from 1 to ', lmax, ':');
+    readln(n)
+  Until (n > 0) And (n <= lmax);
+  Writeln('Enter x, h, a:');
+  Readln(x,h,a);
+  For i := 1 To n Do
+    r[i] := 6 * cos(a * x + i * h);
+  Writeln('Array R of ', n, ' elements²:');
+  For i := 1 To n Do
+    Write(r[i]:8:3, ' ');
+  Writeln;
+  Writeln('Ex. 2');
+  curr_min := 1;
+  For i := 1 To n Do
+    Begin
+      If ( r[i] < r[curr_min] ) Then
+        Begin
+          curr_min := i
+        End;
+    End;
+  writeln('Current minimum: ', curr_min);
+  Writeln('Forming a new array');
+  size_new := curr_min;
+  For i:=curr_min To n Do
+    Begin
+      If (r[i]<0) Then
+        Begin
+          r[size_new] := r[i];
+          size_new := size_new+1;
+        End;
+    End;
+  size_new := size_new-1;
+  If (size_new <> n) Then
+    Begin
+      For i:= 1 To size_new Do
+        Begin
+          Write(r[i]:8:3,' ');
+        End;
+    End
+  Else
+    Begin
+      Writeln('No positive elements after the first global minimum. Nothing to delete');
+    End;
+  writeln();
+  x := 0;
+  Writeln('Ex. 3');
+
+  {Attempt to find the first negative value}
+  first_negative := 0;
+  Repeat
+    first_negative := first_negative+1;
+  Until r[first_negative] < 0 ;
+
+  For i:=(first_negative+1) To (curr_min-1) Do
+    x := x+r[i];
+  Writeln('sum is ',x);
+  x := x/(curr_min-first_negative-1);
+  Writeln('Average is ',x:8:5, 'first negative is', first_negative);
+
+End.
