26ca0feb8396e495c927f1d88b233cc5d6790801
Pavel Shlyak <shlyak-pavel2011@ya.ru>
2017-10-14 00:54:07 +0300

    I didn't get the right idea at first sight. This commit is an attempt to fix that

--- alg_coursera/program.pas
+++ alg_coursera/program.pas
@@ -3,38 +3,44 @@
 Program Coursera;
 
 Var 
-  b, curr: integer;
-  a, c, sum, ex: real;
+  curr: integer;
+  a, b: real;
+  sum, ex, c: double;
 
-Procedure odd(x: real; n: integer; Var z: real);
+Procedure odd(x: real; n: integer; Var z: double);
 Begin
   z := (2*n+1);
   z := 1/(z*exp(z*ln(x)));
 End;
 
-Procedure exact(x: real; n: integer; Var z: real);
+Procedure exact(x: real; n: integer; Var z: double);
 Begin
   z := 0.5*ln((x+1)/(x-1));
 End;
 
 Begin
-  Write('Введите положительное x и количество сложений: ');
+  Write('Введите x > 1 и точность вычислений: ');
   Readln(a, b);
-  While ( b<=0 ) or ( a<=0 ) Do
+  While ( a<=1 ) Or ( b<=0 ) Do
     Begin
       Writeln(
+
 'Юзер, ты тупой. количество сложений - положительное число! Повтори весь ввод'
       );
       Readln(a,b);
     End;
   sum := 0;
-  For curr:=0 To b Do
-    Begin
-      odd(a, curr, c);
-      sum := sum + c;
-    End;
-  exact(a, b, ex);
-  Writeln('Примерный: ', sum);
-  Writeln('Эталонный:', ex);
+  c := 0;
+  curr := 0;
+  Repeat
+    odd(a, curr, c);
+    sum := sum + c;
+    curr := curr + 1;
+  Until (c<=b);
+  curr := curr-1;
+  exact(a, curr, ex);
+  Writeln('Частичная сумма ряда с заданной точностью: ', sum,
+          '. Получена с ', curr, ' интерации.');
+  Writeln('Полная сумма ряда:', ex);
   Writeln('Разность: ',ex-sum)
 End.
