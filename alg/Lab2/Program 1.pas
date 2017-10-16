63242847f60c866acd8138cb91f4cd8a8f0bbf36
Pavel Shlyak <shlyak-pavel2011@ya.ru>
2017-10-16 21:01:27 +0300

    No breaks, no exits... These crazy restrictions

--- alg/Lab2/Program 1.pas
+++ alg/Lab2/Program 1.pas
@@ -1,23 +1,21 @@
-
 Program Laboratory2;
 
-Const 
+Const
   nmax = 200;
   mmax = 200;
   kmax = 200;
 
-Type 
+Type
   matrix = array[1..nmax,1..mmax] Of Integer;
   mas = array [1..kmax] Of Integer;
 
-Var 
+Var
   hasResult: boolean;
   Z: matrix;
   X: mas;
   n, m, k, i, e, maxnum: Integer;
 
 Procedure ReadArray(k: integer; Var X: mas);
-
 Var j: Integer;
 Begin
   For j:=1 To k Do
@@ -25,10 +23,7 @@ Begin
 End;
 
 Procedure ReadMatrix(n: integer; m: integer; Var Z: matrix);
-
-
 {*Эта функция не меняет n и m. Но с пространставми имён надо работать ОСТОРОЖНО*}
-
 Var i,j: integer;
 Begin
   For i:=1 To n Do
@@ -37,7 +32,6 @@ Begin
 End;
 
 Procedure WriteMatrix(n: integer; m: integer; Var Z: matrix);
-
 Var i,j: integer;
 Begin
   For i:=1 To n Do
@@ -49,19 +43,21 @@ Begin
 End;
 
 Function hasElement(element: integer; X: mas; k: integer): boolean;
-
 Var current_element: integer;
+  process: boolean;
 Begin
   hasElement := false;
-  For current_element:=0 To k Do
-    If element=X[current_element] Then
-      Begin
+  process := true;
+  current_element := 0;
+  While (current_element<=k) And process Do
+    Begin
+      If element=X[current_element] Then
         hasElement := true;
-        break;
-      End;
+      current_element := current_element+1;
+    End;
 End;
 
-Begin {this shit}
+Begin
   {Блок инициализации переменных}
   hasResult := false;
   Writeln('Лабораторная работа 2. Программа 1');
@@ -74,7 +70,6 @@ Begin {this shit}
   ReadMatrix(n,m,Z);
   Writeln('А теперь введите элементы массива X');
   ReadArray(k, X);
-
 {Начало, собственно, самого вычислительного алгоритма}
  {Теперь переберём все строки матрицы}
   For i:=1 To m Do
@@ -90,10 +85,10 @@ Begin {this shit}
         End;
   {Зануление максимума при соответствии}
       If Not hasElement(Z[i, maxnum], X, k) Then
-      Begin
-        Z[i, maxnum] := 0;
-		hasResult := true;
-      End;
+        Begin
+          Z[i, maxnum] := 0;
+          hasResult := true;
+        End;
     End;
  {Конец вычисления}
   If hasResult Then
@@ -102,4 +97,4 @@ Begin {this shit}
     Writeln('Преобразований не выполнено. Исходная матрица:'
     );
   writeMatrix(n,m,Z);
-End. {up with this shit}
+End.
