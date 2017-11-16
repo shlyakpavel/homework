a6b87a435d31a2cf7b6058665ef8ad65d75d7edd
Pavel Shlyak <shlyak-pavel2011@ya.ru>
2017-11-16 15:27:13 +0300

    bugfixes for PABC

--- alg/samples/sashaSTR.pas
+++ alg/samples/sashaSTR.pas
@@ -1,4 +1,5 @@
 Program example1;
+
 Const 
   n = 10;
 
@@ -12,25 +13,27 @@ Begin
   writeln('Введите строки');
   For i := 1 To k Do
     readln(T1[i]);
-  n2 := 0; {число подстрок}
-  T2[1] := '';
+  n2 := 1; {число подстрок}
   For i := 1 To k Do
     //k - количество строк
     Begin
       For j:=1 To length(T1[i]) Do
         Begin
-          If (T1[i,j]='*') Then
+          If (T1[i][j]='*') Then
             Begin
-              If (T1[i,j+1]<>'*') Then
-                  Inc(n2)
+              If (T1[i][j-1]<>'*') or (n2=1) Then
+                Begin
+                  Inc(n2);
+                  T2[n2] := '';
+                End;
             End
           Else
-            T2[n2] := concat(T2[n2],T1[i,j]);
+            T2[n2] := concat(T2[n2],T1[i][j]);
         End;
     End;
-  if (n2-1 > 0) then
-	For i := 1 To n2-1 Do
-		writeln(T2[i])
-  else
-	writeln('No substing found');
+  If (n2-1 > 0) Then
+    For i := 2 To n2-1 Do
+      writeln(T2[i])
+      Else
+        writeln('No substing found');
 End.
