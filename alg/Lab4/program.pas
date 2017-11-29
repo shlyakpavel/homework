0015f4195c36e5008ac2cc6c383105da85d7ce4b
Pavel Shlyak <shlyak-pavel2011@ya.ru>
2017-11-29 20:04:40 +0300

    Lab 4. Task 2.5 of 3

--- alg/Lab4/program.pas
+++ alg/Lab4/program.pas
@@ -1,38 +1,41 @@
 Program example1;
-Const
-string_num=10;
 
-Type
-StrList = array[1..string_num] Of string;
+Const 
+  string_num = 10;
 
-Var
-n1, n2, i, j: integer;
-T1, T2: StrList;
-  
-Function matches(var symbol: char) : Boolean;
-begin
-matches:= (symbol = '.') or (symbol = ',') or (symbol = ';') or (symbol = ':')
-end;
+Type 
+  StrList = array[1..string_num] Of string;
 
-Function hasnumsandbrackets(var str: String) : Boolean;
-var
-i: Integer;
-hasnum, hasbrackets: Boolean;
-begin
-hasbrackets:=false;
-hasnum:=false;
-for i:=1 to length(str) do
-begin
-if (hasnum=false) then hasnum:= (str[i] >='0') and (T1[i][j]<='9');
-if (hasbrackets=false) then hasbrackets:= (str[i]='(') or (str[i]=')')
-end;
-hasnumsandbrackets:=hasnum and hasbrackets;
-end;
+Var 
+  n1, n2, i, j: integer;
+  T1, T2: StrList;
 
-Procedure substr(var T1, T2: StrList; n1 : Integer; var n2: Integer);
-var j1: Integer;
-begin
-For i:=1 To n1 Do
+Function matches(Var symbol: char) : Boolean;
+Begin
+  matches := (symbol = '.') Or (symbol = ',') Or (symbol = ';') Or (symbol = ':')
+End;
+
+Function hasnumsandbrackets(Var str: String) : Boolean;
+
+Var 
+  i: Integer;
+  hasnum, hasbrackets: Boolean;
+Begin
+  hasbrackets := false;
+  hasnum := false;
+  For i:=1 To length(str) Do
+    Begin
+      If (hasnum=false) Then hasnum := (str[i] >='0') And (T1[i][j]<='9');
+      If (hasbrackets=false) Then hasbrackets := (str[i]='(') Or (str[i]=')')
+    End;
+  hasnumsandbrackets := hasnum And hasbrackets;
+End;
+
+Procedure substr(Var T1, T2: StrList; n1 : Integer; Var n2: Integer);
+
+Var j1: Integer;
+Begin
+  For i:=1 To n1 Do
     Begin
       j := 1; {номер символа}
       j1 := 1; {начало подстроки}
@@ -56,33 +59,42 @@ For i:=1 To n1 Do
           T2[n2] := copy(T1[i],j1,j-j1);
         End;
     End;
-end;
+End;
 
 Begin
-  writeln('Введите число строк n1');
+  writeln('Введите число строк n1 (не более ',string_num,')');
   readln(n1);
   writeln('Введите строки');
   For i:=1 To n1 Do
     readln(T1[i]);
   n2 := 0; {число подстрок}
   substr(T1, T2, n1, n2);
-  If n2=0 Then {анализ сущестоввания результата для 1 задания}
-    writeln('Нет подстрок')
+  If n2=0 Then
+    {анализ сущестоввания результата для 1 задания}
+    writeln(
+'Нет подстрок. Выполнение всех трёх заданий невозможно'
+    )
   Else
     Begin {вывод результата 1 задания}
       writeln('1. Подстроки');
       For i:=1 To n2 Do
         writeln(T2[i]);
       writeln('2. Первая подстрока, содержащая цифры и круглые скобки:');
-      j:=0;
-      while (i>0) do
-		begin;
-		if hasnumsandbrackets(T2[i]) then
-		  j:=i;
-		Dec(i);
-		end;
-	   if j<>0 then
-	     writeln(T2[j])
-	   else writeln('отсутствует');
+      j := 0;
+      While (i>0) Do
+        Begin;
+          If hasnumsandbrackets(T2[i]) Then
+            j := i;
+          Dec(i);
+        End;
+      If j<>0 Then
+        writeln(T2[j])
+      Else writeln('отсутствует');
+      writeln('3. Вставить три звездочки перед последней цифрой');
+      {Поиск нужной строки}
+      i := 1;
+      While pos(T2[j], T1[i])=0 Do
+        Inc(i);
+      writeln('Строка: ', T1[i]);
     End;
 End.
