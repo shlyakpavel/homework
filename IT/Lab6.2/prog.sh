9f93f70be71e96aabf7ea5c668122604c2c201c4
Pavel Shlyak <shlyak-pavel2011@ya.ru>
2017-12-17 14:23:06 +0300

    XOR XOR XOR XOR

---
+++ IT/Lab6.2/prog.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+read src #чтение открытого текста
+read shifr #чтение закрытого текста
+
+array=(`echo $src | sed -e 's/ /\n/g'`) #превращение открытого текста в массив
+array2=(`echo $shifr | sed -e 's/ /\n/g'`) #превращение закрытого текста в массив
+
+for ((i=0;i<${#array[@]};++i)); do #цикл
+    echo "ibase=16; obase=10; scale=0; xor(${array[i]^^} , ${array2[i]^^})" | bc  -l logic.bc # К=M + C mod 2
+done | xargs
