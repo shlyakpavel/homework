692e8feb8ea579b87c7a688dad961efa4984ec06
Pavel Shlyak <shlyak-pavel2011@ya.ru>
2018-01-17 17:45:23 +0300

    Fixed potential bug found by clang, added 'no result' warnings

--- IT/Lab2Q3/P1.cpp
+++ IT/Lab2Q3/P1.cpp
@@ -4,14 +4,16 @@
  * Вариант 170%45+1=36
  * В заданной целочисленной прямоугольной матрице поменять местами следующие  два элемента:
  * Два первых отрицательных в четных строках.
+ * Тест: seq -3 100 | shuf -r | ./Lab2Q3P1
 */
 
-#define MAXCOLS 10
-#define MAXSTR 15
+#include <iostream>
+
+#define MAXCOLS 45
+#define MAXSTR 25
+
 typedef unsigned int uint;
 typedef unsigned short ushort;
-
-#include <iostream>
 
 using namespace std;
 
@@ -19,22 +21,16 @@ void input(int matrix[MAXSTR][MAXCOLS], ushort *cols, ushort *strings){
     do {
     printf("Введите ненулевое положительное количество строк(<=%d) и столбцов(<=%d)\n", MAXSTR, MAXCOLS);
     scanf("%hu%hu", strings, cols);}
-    while (*strings == 0 || *cols == 0);
+    while (*strings == 0 || *cols == 0 || *strings >= MAXSTR || *cols >= MAXCOLS);
     /* Рассчёт количества элементов в матрице */
     uint els = (*cols) * (*strings);
     printf("Введите матрицу из %d элементов\n", els);
-    //int s, c;
-    /*for (s=0; s < *strings; s++){
-        for (c=0; c < *cols; c++){
-            scanf("%d", &matrix[s][c]);
-        }
-    }*/
     for (uint i=0; i < els; i++)
-        scanf("%d", (int*) matrix + i);
+        scanf("%d", *matrix + i);
 }
 
 int process(int matrix[MAXSTR][MAXCOLS], ushort *cols, ushort *strings){
-    int     fn = -1;   //первое отрицательное
+    int fn = -1;   //первое отрицательное
     for (uint i=0; i < (uint) ((*cols) * (*strings)); i++){
         if ( (i / *cols)%2 )    //если строка чётная
             --i+=*cols;         //то пропустим её
@@ -46,6 +42,7 @@ int process(int matrix[MAXSTR][MAXCOLS], ushort *cols, ushort *strings){
                     *( *matrix + fn) = *( *matrix + i);
                     *( *matrix + i) = tmp;
                     i=(*cols) * (*strings);
+                    fn=-2;
                 }
                 else fn = i;
             }
@@ -65,20 +62,28 @@ void output(int matrix[MAXSTR][MAXCOLS], ushort *cols, ushort *strings){
 int main()
 {
     /* Синхронизация с потоком C нам не нужна*/
-    std::ios::sync_with_stdio(false);
+    ios::sync_with_stdio(false);
     /* Декларирование и инициализация переменных */
     int matrix[MAXSTR][MAXCOLS];
-    ushort *cols = new ushort,
-            *strings = new ushort;
+    ushort  *cols = new ushort(0),
+            *strings = new ushort(0);
     /* Ввод данных */
     input(matrix, cols, strings);
-    output(matrix, cols, strings);
-
     /* Вычисления */
     int ret = process(matrix, cols, strings);
     /* Анализ существования результата */
-    if (ret == 0){};
-    /* Вывод данных */
-    output(matrix, cols, strings);
+    switch ( ret ){
+    case -1:
+        printf("Матрица не содержит отрицательных чисел на нечётных строках\n");
+        break;
+    case -2:
+        output(matrix, cols, strings);  //вывод данных
+        break;
+    default:
+        printf("Недостаточно отрицательных чисел\n");
+    }
+    /* Освобождаем память */
+    delete cols;
+    delete strings;
     return 0;
 }
