388cdcda22cd620609c2b1b4889d685ae6ff30fa
Pavel Shlyak <shlyak-pavel2011@ya.ru>
2018-04-14 17:51:42 +0300

    Some logic

--- IT/sashinkursach/mainwindow.cpp
+++ IT/sashinkursach/mainwindow.cpp
@@ -1,7 +1,8 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 #include <QTableView>
- #include <QStandardItemModel>
+#include <QStandardItemModel>
+#include <math.h>
 
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
@@ -24,4 +25,44 @@ void MainWindow::on_pushButton_2_clicked()
     QList<QStandardItem*> items;
     items.append(new QStandardItem ());
     data->appendColumn(items);
+}
+
+int n = 0;
+int eps = 0;
+
+bool converge(double *xk, double *xkp)
+{
+    double norm = 0;
+    for (int i = 0; i < n; i++)
+    {
+        norm += (xk[i] - xkp[i])*(xk[i] - xkp[i]);
+    }
+    if (sqrt(norm) >= eps)
+        return false;
+    return true;
+}
+
+void MainWindow::on_goButton_clicked()
+{
+
+    eps= ui->accuracyinput->text().toInt();
+    n = data->rowCount();
+    double p[n], x[n];
+    memset(x, 0, n*sizeof(double));
+    do
+    {
+        for (int i = 0; i < n; i++)
+            p[i] = x[i];
+
+        for (int i = 0; i < n; i++)
+        {
+            double var = 0;
+            for (int j = 0; j < i; j++)
+                var += (data->item(i,j)->text().toInt() * x[j]);
+            for (int j = i + 1; j < n; j++)
+                var += (data->item(i,j)->text().toInt() * p[j]);
+            int acc=data->item(i,n)->text().toInt();
+            x[i] = (acc - var) / data->item(i,i)->text().toInt();
+        }
+    } while (!converge(x, p));
 }
